25557a5eea5a5fe381030e54ecc5cc9124bc7156
Brent Cook <busterb@gmail.com>
2026-04-20 06:34:13 -0500

    add timespec operations

--- include/sys/time.h
+++ include/sys/time.h
@@ -40,4 +40,34 @@ static inline int sun_adjtime(struct timeval *delta, struct timeval *olddelta)
 #define adjtime(d, o) sun_adjtime(d, o)
 #endif
 
+#ifndef timespecsub
+/* Operations on timespecs. */
+#define timespecclear(tsp)      (tsp)->tv_sec = (tsp)->tv_nsec = 0
+#define timespecisset(tsp)      ((tsp)->tv_sec || (tsp)->tv_nsec)
+#define timespecisvalid(tsp)                        \
+    ((tsp)->tv_nsec >= 0 && (tsp)->tv_nsec < 1000000000L)
+#define timespeccmp(tsp, usp, cmp)                  \
+    (((tsp)->tv_sec == (usp)->tv_sec) ?             \
+        ((tsp)->tv_nsec cmp (usp)->tv_nsec) :           \
+        ((tsp)->tv_sec cmp (usp)->tv_sec))
+#define timespecadd(tsp, usp, vsp)                  \
+    do {                                \
+        (vsp)->tv_sec = (tsp)->tv_sec + (usp)->tv_sec;      \
+        (vsp)->tv_nsec = (tsp)->tv_nsec + (usp)->tv_nsec;   \
+        if ((vsp)->tv_nsec >= 1000000000L) {            \
+            (vsp)->tv_sec++;                \
+            (vsp)->tv_nsec -= 1000000000L;          \
+        }                           \
+    } while (0)
+#define timespecsub(tsp, usp, vsp)                  \
+    do {                                \
+        (vsp)->tv_sec = (tsp)->tv_sec - (usp)->tv_sec;      \
+        (vsp)->tv_nsec = (tsp)->tv_nsec - (usp)->tv_nsec;   \
+        if ((vsp)->tv_nsec < 0) {               \
+            (vsp)->tv_sec--;                \
+            (vsp)->tv_nsec += 1000000000L;          \
+        }                           \
+    } while (0)
+#endif
+
 #endif
