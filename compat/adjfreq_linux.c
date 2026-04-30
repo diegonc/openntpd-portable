5ab4e4641a482e271d2ca01ca8c4e674ae0ba940
Brent Cook <busterb@gmail.com>
2026-04-29 20:56:53 -0500

    use adjtimex in place of adjtime when it is available

--- compat/adjfreq_linux.c
+++ compat/adjfreq_linux.c
@@ -24,36 +24,6 @@
 #include <ntpd.h>
 
 /*
- * Implement adjtime() via adjtimex() to avoid EINVAL on musl libc when the
- * delta exceeds its supported range, which causes ntpd to behave incorrectly.
- */
-
-int
-adjtime(const struct timeval *delta, struct timeval *olddelta)
-{
-	struct timex txc = { 0 };
-
-	if (delta != NULL) {
-		txc.modes = ADJ_OFFSET_SINGLESHOT;
-		txc.offset = (long)delta->tv_sec * 1000000L + delta->tv_usec;
-	}
-
-	if (adjtimex(&txc) == -1)
-		return (-1);
-
-	if (olddelta != NULL) {
-		olddelta->tv_sec = txc.offset / 1000000L;
-		olddelta->tv_usec = txc.offset % 1000000L;
-		if (olddelta->tv_usec < 0) {
-			olddelta->tv_sec--;
-			olddelta->tv_usec += 1000000L;
-		}
-	}
-
-	return (0);
-}
-
-/*
  * adjfreq (old)freq = nanosec. per seconds shifted left 32 bits
  * timex.freq is ppm / left shifted by SHIFT_USEC (16 bits), defined in timex.h
  */
